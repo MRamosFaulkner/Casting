98972ca80289c5c537b5f37cdc6dbf6df9c7de90
unknown <dnal03@msn.com>
2023-05-08 22:25:07 -0400

    corrections

--- src/Casting.java
+++ src/Casting.java
@@ -1,11 +1,19 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Casting {
     public static void main(String[] args) {
-        double decimalNumber = 112.35;
-        int wholeNumber = 112;
 
+        double decimalNumber = 112.35;
         System.out.println("Double: " + decimalNumber);
-        System.out.println("int: " + wholeNumber);
+
+        int wholeNumber = (int)decimalNumber;
+        System.out.println("int: " + wholeNumber + "\n");
+
+        String numString = "49";
+        System.out.println("The string value is: " + numString);
+
+        int num = Integer.parseInt(numString);
+        System.out.println("The integer value is: " + num);
+
         }
 }
